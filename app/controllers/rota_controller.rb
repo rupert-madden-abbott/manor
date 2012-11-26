90f32afd88992cb6ddd26d1ed21853466da7842d
Rupert <rupert@madden-abbott.com>
2012-11-26 19:40:45 +0000

    Fixed redirection bug on rotum controller, not using nested resource

--- app/controllers/rota_controller.rb
+++ app/controllers/rota_controller.rb
@@ -32,7 +32,7 @@ class RotaController < ApplicationController
     @rotum.assign_duties
     @rotum.save
 
-    redirect_to @rotum, notice: "Duties assigned"
+    redirect_to rotum_duties_url(@rotum), notice: "Duties assigned"
   end
 
   def unassign
@@ -41,20 +41,20 @@ class RotaController < ApplicationController
     @rotum.assigned = false
     @rotum.save
 
-    redirect_to @rotum, notice: "Duties unassigned"
+    redirect_to rotum_duties_url(@rotum), notice: "Duties unassigned"
   end
 
   def publish
     @rotum.published = true
     @rotum.save
 
-    redirect_to @rotum, notice: "Rota published"
+    redirect_to rotum_duties_url(@rotum), notice: "Rota published"
   end
 
   def unpublish
     @rotum.published = false
     @rotum.save
 
-    redirect_to @rotum, notice: "Rota unpublished"
+    redirect_to rotum_duties_url(@rotum), notice: "Rota unpublished"
   end
 end
